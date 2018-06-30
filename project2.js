1288fa6778a587fc667090c6594eed52b92d0780
jbatson44 <bat14009@byui.edu>
2018-06-30 14:35:08 -0600

    login functions and begininng of account creation now works

--- project2.js
+++ project2.js
@@ -15,6 +15,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/getUser', function(request, response) {
+	verifyUser(request, response);
 	getUser(request, response);
 });
 app.get('/makeUser', function(request, response) {
@@ -25,30 +26,53 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+function verifyUser(request, response) {
+	var username = request.query.username;
+	var password = request.query.password;
+	var sql = "SELECT username, password FROM users";
+	pool.query(sql, function(err, results) {
+		console.log("verifying...", results.rows);
+		for (var i = 0; i < results.rows.length; i++) {
+			if (username == results.rows[i].username) {
+				console.log("User exists");
+				if (password == results.rows[i].password) {
+					console.log("password is correct");
+				} else {
+					console.log("password is incorrect");
+				}
+			} else {
+				console.log("Username doesn't exist!");
+			}
+		}
+	});
+}
+
 function getUser(request, response) {
-	//var requestUrl = url.parse(request.url, true);
+	var requestUrl = url.parse(request.url, true);
 	var id = request.query.userid;
+	var username = request.query.username;
+	console.log("user", username);
 	//pool.query('SELECT * FROM users', (err, res) => {
 	console.log("Logging in!")
-	getUserFromDb(id, function(error, result) {
+	getUserFromDb(username, function(error, result) {
 		console.log("We're back! ", result[0]);
 		
-		var username = result[0].username;
+		var userid = result[0].userid;
 		var firstName = result[0].firstname;
 		var lastName = result[0].lastname;
 		var email = result[0].email;
 		var gender = result[0].gender;
 		var city = result[0].city;
 		var state = result[0].state;
-		var param = {userid: id, username: username, firstName: firstName, lastName: lastName, email: email, gender: gender, city: city, state: state};
+		var param = {userid: userid, username: username, firstName: firstName, lastName: lastName, email: email, gender: gender, city: city, state: state};
 		response.render('pages/chat', param);
 	});
 	
 }
-function getUserFromDb(id, callback) {
-	console.log("getUserFromDb called ", id);
-	var sql = "SELECT username, firstName, lastName, email, gender, city, state FROM users WHERE userid = $1::int";
-	var params = [id];
+function getUserFromDb(username, callback) {
+	console.log("getUserFromDb called ", username);
+	var sql = "SELECT userid, firstName, lastName, email, gender, city, state FROM users WHERE username = $1";
+	var params = [username];
 	
 	pool.query(sql, params, function(err, result) {
 		if(err) {
@@ -64,6 +88,18 @@ function getUserFromDb(id, callback) {
 	});
 }
 function makeUser(request, response) {
+	var username = request.query.username;
+	var password = request.query.password;
+	var firstName = request.query.firstName;
+	var lastName = request.query.lastName;
+	var email = request.query.email;
+	var state = request.query.state;
+	var city = request.query.city;
+	var gender = request.query.gender;
 	
+	var sql = "INSERT INTO users(username, password, firstName, lastName, email, state, city, gender) VALUES $1, $2, $3, $4, $5, $6, $7, $8";
+	pool.query(sql, username, password, firstName, lastName, email, state, city, gender, function(err, results) {
+		console.log("Insert successful");
+	});
 }
 
