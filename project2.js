33e962ca29be7bf5d384e4631d9f78aa1df8124a
jbatson44 <bat14009@byui.edu>
2018-07-07 13:39:21 -0600

    milestone 2

--- project2.js
+++ project2.js
@@ -58,18 +58,19 @@ function getUser(request, response) {
 	console.log("user", username);
 	//pool.query('SELECT * FROM users', (err, res) => {
 	console.log("Logging in!")
-	getFriends(username);
+	
 	getUserFromDb(username, function(error, result) {
 		console.log("We're back! ", result[0]);
 		
 		var userid = result[0].userid;
+		var friends = getFriends(userid);
 		var firstName = result[0].firstname;
 		var lastName = result[0].lastname;
 		var email = result[0].email;
 		var gender = result[0].gender;
 		var city = result[0].city;
 		var state = result[0].state;
-		var param = {userid: userid, username: username, firstName: firstName, lastName: lastName, email: email, gender: gender, city: city, state: state};
+		var param = {userid: userid, username: username, firstName: firstName, lastName: lastName, email: email, gender: gender, city: city, state: state, friends: friends};
 		response.render('pages/chat', param);
 	});
 	
@@ -95,16 +96,18 @@ function getUserFromDb(username, callback) {
 function getFriends(userId) {
 	var sql = "SELECT friendId FROM friend WHERE userId = $1";
 	var params = [userId];
+	var result;
 	pool.query(sql, params, function(err, result) {
 		if(err) {
-			console.log("ERROR: ");
+			console.log("ERROR with friends: ");
 			console.log(err);
 			return 0;
 		}
 		
-		console.log("Found result: " + JSON.stringify(result.rows));
-		
+		console.log("Found friendIds: " + JSON.stringify(result.rows));
+		result = result.rows;
 	});
+	return result;
 }
 
 function makeUser(request, response) {
